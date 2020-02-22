ea76abc348df70ce853ef8de139a2441fd7f5fd7
Ana Sequeira <anacseq@gmail.com>
2020-02-22 11:41:16 +0000

    aux

--- src/app/building-detail/building-detail.component.ts
+++ src/app/building-detail/building-detail.component.ts
@@ -7,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BuildingDetailComponent implements OnInit {
 
-  constructor() { }
+
+  constructor() { 
+    let aux= true;
+  }
 
   ngOnInit() {
   }

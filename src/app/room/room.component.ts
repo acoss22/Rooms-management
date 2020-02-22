29bb1cc175d3274ba8fd96fe1376252ac711cbc0
andrefnb <andrefnbenevides@gmail.com>
2020-02-22 11:28:17 +0000

    new var

--- src/app/room/room.component.ts
+++ src/app/room/room.component.ts
@@ -7,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RoomComponent implements OnInit {
 
-  constructor() { }
+  constructor() { 
+    let banana = "";
+  }
 
   ngOnInit() {
   }

59befcd83df8da6fb3d8b207dc7ef482bd419ba9
Ana Sequeira <anacseq@gmail.com>
2020-04-03 15:49:27 +0100

    local storage testing

--- src/app/building-list/building.edit.component.ts
+++ src/app/building-list/building.edit.component.ts
@@ -1,26 +1,31 @@
-import { Component, OnInit, ElementRef, ViewChild, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild, EventEmitter, Output, Inject, Injectable } from '@angular/core';
 import { Room } from '../../shared/room.model';
 import { FormsModule, ReactiveFormsModule, NgForm } from '@angular/forms';
 import { Building } from 'src/shared/building.model';
+import { NgModule } from '@angular/core';
+import { StorageServiceModule, LOCAL_STORAGE, StorageService } from 'ngx-webstorage-service';
 
+@NgModule({
+  imports: [StorageServiceModule]
+})
 
 @Component({
   selector: 'app-building-edit',
   templateUrl: './building-edit.component.html'
 })
+
+@Injectable()
 export class BuildingEditComponent implements OnInit {
+  // key that is used to access the data in local storageconst 
+  STORAGE_KEY = 'local_todolist';
+  @ViewChild('f', { static: false }) buildingForm: NgForm;
+  buildingList = [];
+  generatedID: number = 1;
+  building: Building = new Building(this.generatedID, '', '', '');
 
-  @ViewChild('f', {static: false}) buildingForm: NgForm;
-  
+  submitted = false;
 
-  generatedID : number = 1;
-
-  building : Building = new Building(this.generatedID,'','','');
-
-
-submitted = false;
-
-  constructor() { }
+  constructor(@Inject(LOCAL_STORAGE) private storage: StorageService) { }
 
   ngOnInit() {
   }
@@ -29,21 +34,42 @@ submitted = false;
   onSubmit() {
     console.log("object submitted");
 
-    this.submitted=true;
-  
+    this.submitted = true;
+
     let currentBuilding = this.buildingForm.value;
 
-    console.log(typeof currentBuilding);
-    console.log(JSON.stringify(currentBuilding));
-    let currentBuildingName = currentBuilding['ngModelGroup'].buildingName;
-    console.log(currentBuildingName);
 
+    let currentBuildingName = currentBuilding['ngModelGroup'].buildingName;
+    let currentBuildingDescription = currentBuilding['ngModelGroup'].buildingDescription;
+    let currentBuildingImagePath = '';
+
+    this.building.id = this.generatedID++;
     this.building.name = currentBuildingName;
-    this.building.description = this.buildingForm.value.buildingDescription;
-    this.building.imagePath = this.buildingForm.value.imagePath;
+    this.building.description = currentBuildingDescription;
+    this.building.imagePath = currentBuildingImagePath;
+
+    console.log(this.building);
+    this.storeOnLocalStorage(this.building);
     this.buildingForm.reset();
-   // console.log(this.building);
+
 
   }
 
+  public storeOnLocalStorage(currentBuilding: Building): void {
+
+    // get array of tasks from local storage
+    this.buildingList = this.storage.get(this.STORAGE_KEY) || [];
+    // push new task to array
+    this.buildingList.push({
+      currentBuilding
+    });
+
+    // insert updated array to local storage
+    this.storage.set(this.STORAGE_KEY, this.buildingList);
+  
+    console.log(this.storage.get(this.STORAGE_KEY) || 'LocaL storage is empty');
+  }
 }
+
+
+
